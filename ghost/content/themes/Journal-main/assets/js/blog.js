24e972d243ca63394bd17d35a4b0bdafdf51ca98
tomnyson <tabletkindfire@gmail.com>
2021-08-10 21:22:31 +0700

    update blog first time

--- ghost/content/themes/Journal-main/assets/js/blog.js
+++ ghost/content/themes/Journal-main/assets/js/blog.js
@@ -7,3 +7,33 @@ document.addEventListener('DOMContentLoaded', (event) => {
     hljs.highlightBlock(block);
   });
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+  var lazyloadImages = document.querySelectorAll("img.lazy");    
+  var lazyloadThrottleTimeout;
+  
+  function lazyload () {
+    if(lazyloadThrottleTimeout) {
+      clearTimeout(lazyloadThrottleTimeout);
+    }    
+    
+    lazyloadThrottleTimeout = setTimeout(function() {
+        var scrollTop = window.pageYOffset;
+        lazyloadImages.forEach(function(img) {
+            if(img.offsetTop < (window.innerHeight + scrollTop)) {
+              img.src = img.src;
+              img.classList.remove('lazy');
+            }
+        });
+        if(lazyloadImages.length == 0) { 
+          document.removeEventListener("scroll", lazyload);
+          window.removeEventListener("resize", lazyload);
+          window.removeEventListener("orientationChange", lazyload);
+        }
+    }, 20);
+  }
+  
+  document.addEventListener("scroll", lazyload);
+  window.addEventListener("resize", lazyload);
+  window.addEventListener("orientationChange", lazyload);
+});
